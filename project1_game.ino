5b315caa80d69e71a62f245a01ba69609c2935a8
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-10-07 00:13:21 -0400

    1st version done

--- project1_game.ino
+++ project1_game.ino
@@ -10,13 +10,35 @@ unsigned long shootStartTime;
 unsigned long globalTimer;
 unsigned long globalStartTime;
 
+unsigned long gameTimer;
+unsigned long gameStartTime;
+
+unsigned long gameoverTimer;
+unsigned long gameoverStartTime;
+boolean lose = false;
+boolean shoot = false;
+
 const int poten = A7;
 int potenValue;
+int gameSpeed;
 
 const int btn = 13;
 boolean btnReading;
 
 byte currentCol[8] = {0,0,0,0,0,0,0,0};
+byte gameOverFace1[8] = {
+  0,
+  B10100101,
+  B01000010,
+  B10100101,
+  0,
+  0,
+  B00011000,
+  B00100100
+  };
+
+byte gameOverFace2[8] = {0,0,0,0,0,0,0,0};
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,18 +54,19 @@ void setup() {
 }
 
 void Frame (byte col_values[]) {
+  const int delayTime = 1;
   PORTC = B111111;
   PORTB = B11;
   for(byte x = 0; x<6; x++) {
     PORTC ^= (B100000>>x);
     PORTD = col_values[x];
-    delay(1);
+    delay(delayTime);
     PORTC = B111111;
   }
   for(byte x=0; x<2; x++) {
     PORTB ^= B10>>x;
     PORTD = col_values[x+6];
-    delay(1);
+    delay(delayTime);
     PORTB = B11;
   }
 }
@@ -113,11 +136,16 @@ void LedPlant(byte colArray[], int interval) {
     colArray[3] = B10000000;
     colArray[2] = B10000000;
     colArray[1] = B10000000;
-  } else {
+  } else if(shoot == true) {
     ledPlantStartTime = millis();
     for (byte i=1; i<7; i++) {
       colArray[i] = 0;
     }
+  } else {
+    GameOver();
+    gameStartTime = millis();
+    lose = false;
+    Reset(currentCol);
   }
 }
 
@@ -142,17 +170,55 @@ void ShootControl(byte colArray[]) {
   for(byte i=1; i<8; i++) {
     colArray[i] = colArray[i+1];
   } 
+  colArray [7] = 0;
 }
 
 void Reset(byte colArray[]) {
   ledPlantStartTime = millis();
+  topBarStartTime = millis();
+  shoot = false;
+}
+
+ void LoseOrNot(byte colArray[]) {
+  if(colArray[0] == (colArray[0] | colArray[1]) ) {
+    lose = true;
+  } else {
+    lose = false;
+  }
+}
+
+void GameOver() {
+  gameoverStartTime = millis();
+  gameoverTimer = millis() - gameoverStartTime;
+  while(gameoverTimer <= 2000){
+    gameoverTimer = millis() - gameoverStartTime;
+    if(gameoverTimer <= 500) {
+      Frame(gameOverFace1);
+    } else if(gameoverTimer <= 1000) {
+      Frame(gameOverFace2);
+    } else if(gameoverTimer <= 1500) {
+      Frame(gameOverFace1);
+    } else {
+      Frame(gameOverFace2);
+    }
+  }
 }
   
 void loop() {
-  btnReading = digitalRead(btn);
   
+  gameTimer = millis()-gameStartTime;
+  if(gameTimer <= 15000) {
+    gameSpeed = 1000;
+  } else if (gameTimer <= 30000) {
+    gameSpeed = 200;
+  } else {
+    gameSpeed = 100;
+  }
+  
+  btnReading = digitalRead(btn);
   if(btnReading == HIGH) {
-    for(byte i=0;i<8;i++) {
+    shoot = true;
+    for(byte i=0;i<6;i++) {
       ShootControl(currentCol);
       while(globalTimer<=10) {
         globalTimer = millis()-globalStartTime;
@@ -161,10 +227,16 @@ void loop() {
       }
       globalStartTime = millis();
       globalTimer = millis()-globalStartTime;
-    };
+    }
+    LoseOrNot(currentCol);
+    if(lose == true) {
+      GameOver();
+      gameStartTime = millis();
+      lose = false;
+    }
     Reset(currentCol); 
   } else {
-    TopBar(currentCol,500);
+    TopBar(currentCol,gameSpeed);
     LedPlant(currentCol, 1000);
     ControlPlant(currentCol);
     Frame(currentCol);
--- project1_testground/project1_testground.ino
+++ project1_testground/project1_testground.ino
@@ -4,12 +4,41 @@ unsigned long topBarStartTime;
 unsigned long ledPlantTimer;
 unsigned long ledPlantStartTime;
 
+unsigned long shootTimer;
+unsigned long shootStartTime;
+
+unsigned long globalTimer;
+unsigned long globalStartTime;
+
+unsigned long gameTimer;
+unsigned long gameStartTime;
+
+unsigned long gameoverTimer;
+unsigned long gameoverStartTime;
+boolean lose = false;
+boolean shoot = false;
+
 const int poten = A7;
 int potenValue;
+int gameSpeed;
 
+const int btn = 13;
+boolean btnReading;
 
 byte currentCol[8] = {0,0,0,0,0,0,0,0};
-byte colTest[8] = {B1000,B1000,B1000,B1000,B1000,B1000,B1000,B1000};
+byte gameOverFace1[8] = {
+  0,
+  B10100101,
+  B01000010,
+  B10100101,
+  0,
+  0,
+  B00011000,
+  B00100100
+  };
+
+byte gameOverFace2[8] = {0,0,0,0,0,0,0,0};
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,21 +49,24 @@ void setup() {
   PORTC = B0;
   PORTB = B0;
   pinMode(poten, INPUT);
+  pinMode(btn, INPUT);
+  //Serial.begin(9600);
 }
 
 void Frame (byte col_values[]) {
+  const int delayTime = 1;
   PORTC = B111111;
   PORTB = B11;
   for(byte x = 0; x<6; x++) {
     PORTC ^= (B100000>>x);
     PORTD = col_values[x];
-    delay(1);
+    delay(delayTime);
     PORTC = B111111;
   }
   for(byte x=0; x<2; x++) {
     PORTB ^= B10>>x;
     PORTD = col_values[x+6];
-    delay(1);
+    delay(delayTime);
     PORTB = B11;
   }
 }
@@ -72,46 +104,141 @@ void LedPlant(byte colArray[], int interval) {
   if(ledPlantTimer <= interval) {
     colArray[7] = B10000000;
   } else if(ledPlantTimer <= interval*2) {
+    colArray[7] = B10000000;
     colArray[6] = B10000000;
   } else if(ledPlantTimer <= interval*3) {
+    colArray[7] = B10000000;
+    colArray[6] = B10000000;
     colArray[5] = B10000000;
   } else if(ledPlantTimer <= interval*4) {
+    colArray[7] = B10000000;
+    colArray[6] = B10000000;
+    colArray[5] = B10000000;
     colArray[4] = B10000000;
   } else if(ledPlantTimer <= interval*5) {
+    colArray[7] = B10000000;
+    colArray[6] = B10000000;
+    colArray[5] = B10000000;
+    colArray[4] = B10000000;
     colArray[3] = B10000000;
   } else if(ledPlantTimer <= interval*6) {
+    colArray[7] = B10000000;
+    colArray[6] = B10000000;
+    colArray[5] = B10000000;
+    colArray[4] = B10000000;
+    colArray[3] = B10000000;
     colArray[2] = B10000000;
   } else if(ledPlantTimer <= interval*7) {
+    colArray[7] = B10000000;
+    colArray[6] = B10000000;
+    colArray[5] = B10000000;
+    colArray[4] = B10000000;
+    colArray[3] = B10000000;
+    colArray[2] = B10000000;
     colArray[1] = B10000000;
-  } else {
+  } else if(shoot == true) {
     ledPlantStartTime = millis();
     for (byte i=1; i<7; i++) {
       colArray[i] = 0;
     }
+  } else {
+    GameOver();
+    gameStartTime = millis();
+    lose = false;
+    Reset(currentCol);
   }
 }
 
 void ControlPlant(byte colArray[]) {
   potenValue = analogRead(poten);
-  potenValue = map(potenValue, 0, 1023, 0, 7);
+  potenValue = map(potenValue,1023, 0, 0, 7);
   /*for(byte i=7; i>0; i--) {
     colArray[i] = colArray[i] >> potenValue;
   }*/
+  colArray[1] = colArray[1] >> potenValue;
+  colArray[2] = colArray[2] >> potenValue;
+  colArray[3] = colArray[3] >> potenValue;
+  colArray[4] = colArray[4] >> potenValue;
+  colArray[5] = colArray[5] >> potenValue;
+  colArray[6] = colArray[6] >> potenValue;
+  colArray[7] = colArray[7] >> potenValue;
+}
 
-  colArray[0] >>= 1;
-  colArray[1] >>0;
-  colArray[2] >>1;
-  colArray[3] = B1000>>0;
-  colArray[4] = B1000 >>1;
-  colArray[5] = B1000 >>0;
-  colArray[6] = colArray[6] >>1;
-  colArray[7] = colArray[7] >>0;
+void ShootControl(byte colArray[]) {
+  
+  //Serial.println(btnReading);
+  for(byte i=1; i<8; i++) {
+    colArray[i] = colArray[i+1];
+  } 
+  colArray [7] = 0;
+}
+
+void Reset(byte colArray[]) {
+  ledPlantStartTime = millis();
+  topBarStartTime = millis();
+  shoot = false;
+}
+
+ void LoseOrNot(byte colArray[]) {
+  if(colArray[0] == (colArray[0] | colArray[1]) ) {
+    lose = true;
+  } else {
+    lose = false;
+  }
+}
+
+void GameOver() {
+  gameoverStartTime = millis();
+  gameoverTimer = millis() - gameoverStartTime;
+  while(gameoverTimer <= 2000){
+    gameoverTimer = millis() - gameoverStartTime;
+    if(gameoverTimer <= 500) {
+      Frame(gameOverFace1);
+    } else if(gameoverTimer <= 1000) {
+      Frame(gameOverFace2);
+    } else if(gameoverTimer <= 1500) {
+      Frame(gameOverFace1);
+    } else {
+      Frame(gameOverFace2);
+    }
+  }
 }
   
 void loop() {
-  //TopBar(currentCol,500);
-  //LedPlant(currentCol, 1000);
-  ControlPlant(colTest);
   
-  Frame(colTest);
+  gameTimer = millis()-gameStartTime;
+  if(gameTimer <= 15000) {
+    gameSpeed = 1000;
+  } else if (gameTimer <= 30000) {
+    gameSpeed = 200;
+  } else {
+    gameSpeed = 100;
+  }
+  
+  btnReading = digitalRead(btn);
+  if(btnReading == HIGH) {
+    shoot = true;
+    for(byte i=0;i<6;i++) {
+      ShootControl(currentCol);
+      while(globalTimer<=10) {
+        globalTimer = millis()-globalStartTime;
+        Frame(currentCol);
+        //Serial.println(currentCol[1]);
+      }
+      globalStartTime = millis();
+      globalTimer = millis()-globalStartTime;
+    }
+    LoseOrNot(currentCol);
+    if(lose == true) {
+      GameOver();
+      gameStartTime = millis();
+      lose = false;
+    }
+    Reset(currentCol); 
+  } else {
+    TopBar(currentCol,gameSpeed);
+    LedPlant(currentCol, 1000);
+    ControlPlant(currentCol);
+    Frame(currentCol);
+  }
 }
